640819cdeee11df44a1b35c056b4a1f59cae12df
pranavbhatthero <pbhatt@salesforce.com>
2020-05-04 00:52:40 -0400

    Second Commit with new button

--- force-app/main/default/lwc/displayAccountIntelligence/displayAccountIntelligence.js
+++ force-app/main/default/lwc/displayAccountIntelligence/displayAccountIntelligence.js
@@ -1,22 +1,20 @@
 import { LightningElement, api, wire } from 'lwc';
-import { CurrentPageReference } from 'lightning/navigation';
 import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
-
 import getJobsFromHerokuWS from '@salesforce/apex/TalktoMyNewHerokuWebService.getJobsFromHerokuWS';
-
+import postAccounttoHerokuWS from '@salesforce/apex/PosttoMyNewHerokuWebService.postAccounttoHerokuWS';
 import ACCOUNT_NAME_FIELD from '@salesforce/schema/Account.Name';
 
-//const TICKER_FIELD = ['Account.TickerSymbol'];
 
 export default class DisplayAccountIntelligence extends LightningElement {
 
     jobs;
+    jobsExecutives;
     error;
     my_name;
+    postHappen;
     
     @api recordId;
-    //@wire(CurrentPageReference) pageRef;
 
     @wire(getRecord, { recordId: '$recordId', fields: [ACCOUNT_NAME_FIELD] })
     wiredRecord({ error, data }) {
@@ -32,27 +30,89 @@ export default class DisplayAccountIntelligence extends LightningElement {
             this.my_name = getFieldValue(data, ACCOUNT_NAME_FIELD);
         }
     }
-           
-    handleSearchClick() {
-        //console.log("My name my name ... ", this.my_name);
-        getJobsFromHerokuWS({ account_name: this.my_name})
-            .then(result => {
-                const output = JSON.parse(result);
-                this.error = undefined
-                //console.log("Get them Jobs ... ", output);
 
+    handleSearchClick() {
+        console.log("My name is ... my name is - handleSearchClick ...", this.my_name);
+        getJobsFromHerokuWS({ account_name: this.my_name, resource_path: 'categories'})
+            .then(result => {
+                console.log("result for jobs ... ", result.length);
+                const output = JSON.parse(result);
+                console.log("Read length for jobs ... ", output.length);
+                this.error = undefined;
                 if (output !== undefined) {
-                    this.jobs = output;
-                    console.log("Read my books for jobs ... ", this.jobs);
-                    /*
-                    this.dispatchEvent(
-                        new ShowToastEvent({
-                            title: 'Success',
-                            message: 'Account created',
-                            variant: 'success',
-                        }),
-                    );
-                    */
+                    if (output.length === 3) {
+                        // postAccounttoHerokuWS() method
+                        this.postToHerokuWebService();
+                    }
+                    else {
+                        this.jobsExecutives = null;
+                        this.jobs = output;
+                        //console.log('so my POST request works and is going in final if statement');
+                        console.log("Read my books for jobs ... ", this.jobs);    
+                    }
+                } 
+                else {
+                    console.log("No info returned in call back in selectLocation method");
+                }
+            })
+            .catch(error => {
+                this.message = undefined;
+                this.error = error;
+                
+                console.log("error", JSON.stringify(this.error));
+            });
+    }
+
+
+
+    handleJobsCategoriesSearchClick() {
+        console.log("My name is ... my name is - handleJobsCategoriesSearchClick ...", this.my_name);
+        getJobsFromHerokuWS({ account_name: this.my_name, resource_path: 'executives'})
+            .then(result => {
+                console.log("result for jobs ... ", result.length);
+                const output = JSON.parse(result);
+                console.log("Read length for jobs ... ", output.length);
+                this.error = undefined;
+                if (output !== undefined) {
+                    if (output.length === 0) {
+                        // postAccounttoHerokuWS() method
+                        this.postToHerokuWebService();
+                    }
+                    else {
+                        this.jobs = null;
+                        this.jobsExecutives = output;
+                        //console.log('so my POST request works and is going in final if statement');
+                        console.log("Read my books for jobs ... ", this.jobsExecutives);
+                    }
+                } 
+                else {
+                    console.log("No info returned in call back in selectLocation method");
+                }
+            })
+            .catch(error => {
+                this.message = undefined;
+                this.error = error;
+                
+                console.log("error", JSON.stringify(this.error));
+            });
+    }
+
+
+    postToHerokuWebService() {
+        // postAccounttoHerokuWS() method
+        console.log("My name is ... my name is - postToHerokuWebService ...", this.my_name);
+        postAccounttoHerokuWS({
+                account_name: this.my_name,
+            })
+            .then(result => {
+                console.log("result for post ... ", result.length);
+                const output = JSON.parse(result);
+                console.log("Read length for post ... ", output.length);
+                this.error = undefined;
+                if (output !== undefined) {
+                    this.postHappen = output;
+                    //console.log('so my POST request works and is going in final if statement');
+                    console.log("Read my post for somethign ... ", this.postHappen);
                 } else {
                     console.log("No info returned in call back in selectLocation method");
                 }
@@ -60,16 +120,10 @@ export default class DisplayAccountIntelligence extends LightningElement {
             .catch(error => {
                 this.message = undefined;
                 this.error = error;
-                /*
-                this.dispatchEvent(
-                    new ShowToastEvent({
-                        title: 'Error creating record',
-                        message: error.body.message,
-                        variant: 'error',
-                    }),
-                );
-                */
+
                 console.log("error", JSON.stringify(this.error));
             });
     }
+
 }
+
